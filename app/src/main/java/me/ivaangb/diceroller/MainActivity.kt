02bbc146f6113559254bd326bb870cc601f2abf7
Ivan Gonzalez <gonzalezbarrazaivan@gmail.com>
2021-03-22 14:04:07 -0500

    Third Challenge: Added a second dice. Added image assets for dices. Added "Clear" button which set dices to 0.

--- app/src/main/java/me/ivaangb/diceroller/MainActivity.kt
+++ app/src/main/java/me/ivaangb/diceroller/MainActivity.kt
@@ -2,49 +2,57 @@ package me.ivaangb.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Xml
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var firstDiceImage : ImageView
+    lateinit var secondDiceImage : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val resultText: TextView = findViewById(R.id.result_text)
-
 
         val rollButon: Button = findViewById(R.id.roll_button)
-        val countUp: Button = findViewById(R.id.countUp_Button)
-        val resetButton: Button = findViewById(R.id.reset_button)
+        val clearButton: Button = findViewById(R.id.clear_button)
 
+        firstDiceImage = findViewById(R.id.first_dice)
+        secondDiceImage = findViewById(R.id.second_dice)
 
-        rollButon.setOnClickListener { rollDice(resultText) }
-        countUp.setOnClickListener { countUp(resultText) }
-        resetButton.setOnClickListener {reset(resultText)}
+        rollButon.setOnClickListener { rollDice() }
+        clearButton.setOnClickListener { clearButton() }
     }
 
 
-    private fun rollDice(resultText: TextView) {
+
+    private fun rollDice() {
+        firstDiceImage.setImageResource(getRandomDiceImage())
+        secondDiceImage.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int {
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
-    }
 
-    private fun countUp(resultText: TextView) {
-        var counter = 1
-        if (resultText.text.toString().toIntOrNull() == null) {
-            resultText.text = counter.toString()
-        } else {
-            counter = resultText.text.toString().toInt()
-
-            if (counter < 6) {
-                counter++
-                resultText.text = counter.toString()
-            }
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 
-    private fun reset(resultText: TextView) {
-        resultText.text = "0"
+    private fun clearButton() {
+        val emptyDice: Int = R.drawable.empty_dice
+        firstDiceImage.setImageResource(emptyDice)
+        secondDiceImage.setImageResource(emptyDice)
     }
+
+
 
 }
